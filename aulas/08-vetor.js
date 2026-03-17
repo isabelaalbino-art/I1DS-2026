54c05351c2b2cd522a5625d1ba3f68313113f1c3
isabelaalbino-art <isabela.albino@edu.senai.br>
2026-03-17 14:09:26 -0300

    Remover um elemento do inicio e do final do vetor, splice, retornar um indice do elemento do vetor

--- aulas/08-vetor.js
+++ aulas/08-vetor.js
@@ -27,3 +27,51 @@ console.log("O Vetor agora tem", dinos.length, "elementos");
 console.log("1ª posição:", dinos[0]);
 console.log("4ª posição:", dinos[3]);
 console.log("20ª posição: (não existe);", dinos[20]);
+
+// pop -> Remover um elemento do final do vetor (ultimo elemento)
+dinos.pop();
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos");
+
+//shift -> Remove um elemento do inicio do vetor (primeiro elemento)
+dinos.shift();
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos");
+
+//splice -> Remove um elemento a partir de um indice, e tambem precisa ser informada a quantidade de registro a ser excluido a partir de um indice
+dinos.splice(1, 1); // Remove somente o segundo item da lista
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos");
+
+// Adicionar mais elemento ao vetor
+dinos.push("Pterodáctio");
+dinos.push("Estegossauro");
+dinos.push("Argentinossauro");
+
+//Procurar um elemento a partir do nome (descrição)
+//IndexOf (procurado) -> retorna o indice do elementono vetor
+let procurado = "Estegossauro";
+let indice = dinos.indexOf(procurado);
+console.log("O", procurado, "está no indice:", indice);
+// indexOf(procurado -> retorna -1 caso não encontre o elemento
+    procurado ="Cristianossauro"
+indice = dinos.indexOf(procurado);
+console.log("O", procurado, "está no indice:", indice);
+
+// Excluir um elemento com base em seu nome
+// 1º localizar o indice do elemento a partir do nome 
+// 2º excluir o registro com base no indice retornado
+
+
+procurado = "Estegossauro";
+indice = dinos.indexOf(procurado);
+console.log("O", procurado, "está no indice:", indice);
+dinos.splice (indice, 1);
+console.table(dinos);
+
+// Alterar o valor de um elemento a partir do indice 
+dinos[1] = "Anquilossauro"
+console.table(dinos);
+
+
+
